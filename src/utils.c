34356bbea4198487bb4ea479c8a25f7736a3b342
Rémi Attab <remi.attab@adgear.com>
2017-09-20 18:16:53 -0400

    Avoid dirent.d_type in rill_scan_dir

--- src/utils.c
+++ src/utils.c
@@ -13,16 +13,30 @@
 // scan_dir
 // -----------------------------------------------------------------------------
 
+bool is_rill_file(const char *name)
+{
+    static const char ext[] = ".rill";
+
+    size_t len = strnlen(name, NAME_MAX);
+    if (len < sizeof(ext)) return false;
+
+    return !strcmp(name + (len - sizeof(ext) + 1), ext);
+}
+
 size_t rill_scan_dir(const char *dir, struct rill_store **list, size_t cap)
 {
     DIR *dir_handle = opendir(dir);
-    if (!dir_handle) return 0;
+    if (!dir_handle) {
+        if (errno == ENOENT) return 0;
+        fail_errno("unable to open dir '%s'", dir);
+        return 0;
+    }
 
     size_t len = 0;
     struct dirent *entry = NULL;
     while ((entry = readdir(dir_handle))) {
-        if (entry->d_type != DT_REG) continue;
-        if (!strcmp(entry->d_name, "acc")) continue;
+        // I found the one filesystem that doesn't support dirent->d_type...
+        if (!is_rill_file(entry->d_name)) continue;
 
         char file[PATH_MAX];
         snprintf(file, sizeof(file), "%s/%s", dir, entry->d_name);
@@ -40,4 +54,3 @@ size_t rill_scan_dir(const char *dir, struct rill_store **list, size_t cap)
     closedir(dir_handle);
     return len;
 }
-
